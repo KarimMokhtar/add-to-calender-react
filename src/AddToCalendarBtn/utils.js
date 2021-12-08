3b60646fe3ddcea72ea5651245051f51bdbcbcd6
Karim Mokhtar <karimmokhtar28@gmail.com>
2021-12-08 21:04:30 +0200

    add google time zone and apple download

--- src/AddToCalendarBtn/utils.js
+++ src/AddToCalendarBtn/utils.js
@@ -1,93 +1,126 @@
+export const MEETING_DATA = {
+  zoom_id: "123456",
+  zooom_password: "465646",
+  interviewer: "Zeina Abdulsamad",
+  startAt: "2021-12-20T10:00:00Z",
+  endAt: "2021-12-20T11:00:00Z",
+  details: `Event details go here`,
+  timeZone: "Europe/Berlin",
+};
+
 export const CALENDAR_OPTIONS = [
   {
-    label: 'Google',
-    value: 'google'
+    label: "Google",
+    value: "google",
   },
   {
-    label: 'Outlook',
-    value: 'outlook'
+    label: "Outlook",
+    value: "outlook",
   },
   {
-    label: 'Yahoo',
-    value: 'yahoo'
+    label: "Apple",
+    value: "apple",
   },
   {
-    label: 'ics',
-    value: 'ics'
-  }
+    label: "ics",
+    value: "ics",
+  },
 ];
 
-const generateDuration = (event) => {
+const generateDuration = event => {
   const minutes = Math.floor((+new Date(event.endsAt) - +new Date(event.startsAt)) / 60 / 1000);
   return `${Math.floor(minutes / 60)}:${`0${minutes % 60}`.slice(-2)}`;
 };
 
-const formatTime = (time) => new Date(time).toISOString().replace(/[-:]|\.\d{3}/g, '');
+const formatTime = time => new Date(time).toISOString().replace(/[-:]|\.\d{3}/g, "");
+const formatOutlookTime = time => new Date(time).toISOString();
 
-const buildURL = (base, query) => Object.keys(query).reduce(
-  (accum, key, index) => {
+const buildURL = (base, query) =>
+  Object.keys(query).reduce((accum, key, index) => {
     const value = query[key];
 
     if (value !== null) {
-      return `${accum}${index === 0 ? '?' : '&'}${key}=${encodeURIComponent(value)}`;
+      return `${accum}${index === 0 ? "?" : "&"}${key}=${encodeURIComponent(value)}`;
     }
     return accum;
-  },
-  base
-);
+  }, base);
 
-const buildGoogleCalendarUrl = (event) => buildURL('https://calendar.google.com/calendar/render', {
-  action: 'TEMPLATE',
-  dates: `${formatTime(event.startsAt)}/${formatTime(event.endsAt)}`,
-  location: event.location,
-  text: event.name,
-  details: event.details,
-});
+const buildGoogleCalendarUrl = event =>
+  buildURL("https://calendar.google.com/calendar/render", {
+    action: "TEMPLATE",
+    dates: `${formatTime(event.startsAt)}/${formatTime(event.endsAt)}`,
+    location: event.location,
+    text: event.name,
+    details: event.details,
+    ctz: event.timeZone,
+  });
 
-const buildOutlookCalendarUrl = (event) => buildURL('https://outlook.live.com/owa', {
-  rru: 'addevent',
-  startdt: formatTime(event.startsAt),
-  enddt: formatTime(event.endsAt),
-  subject: event.name,
-  location: event.location,
-  body: event.details,
-  allday: false,
-  uid: new Date().getTime().toString(),
-  path: '/calendar/view/Month',
-});
+const buildOutlookCalendarUrl = event =>
+  buildURL("https://outlook.live.com/owa", {
+    rru: "addevent",
+    startdt: formatOutlookTime(event.startsAt),
+    enddt: formatOutlookTime(event.endsAt),
+    subject: event.name,
+    location: event.location,
+    body: event.details,
+    allday: false,
+    uid: new Date().getTime().toString(),
+    path: "/calendar/view/Month",
+  });
 
-const buildYahooCalendarUrl = (event) => buildURL('https://calendar.yahoo.com', {
-  v: 60,
-  view: 'd',
-  type: 20,
-  title: event.name,
-  st: formatTime(event.startsAt),
-  dur: generateDuration(event),
-  desc: event.details,
-  in_loc: event.location,
-});
+const buildYahooCalendarUrl = event =>
+  buildURL("https://calendar.yahoo.com", {
+    v: 60,
+    view: "d",
+    type: 20,
+    title: event.name,
+    st: formatTime(event.startsAt),
+    dur: generateDuration(event),
+    desc: event.details,
+    in_loc: event.location,
+  });
 
-const buildICSCalendarUrl = (event) => {
+const buildAppleCalendarUrl = event => {
   const components = [
-    'BEGIN:VCALENDAR',
-    'VERSION:2.0',
-    'BEGIN:VEVENT',
+    "VERSION:2.0",
+    "CALSCALE:GREGORIAN",
+    "BEGIN:VEVENT",
+    `URL:${document.URL}`,
+    "STATUS:CONFIRMED",
+    "SEQUENCE:0",
+    `DTSTART:${formatTime(event.startsAt)}`,
+    `DTEND:${formatTime(event.endsAt)}`,
+    `SUMMARY:${event.name}`,
+    `DESCRIPTION:${event.details}`,
+    `LOCATION:${event.location}`,
+    "END:VEVENT",
+    "END:VCALENDAR",
+  ];
+
+  return encodeURI(`data:text/calendar;charset=utf8,${components.join("\n")}`);
+};
+
+const buildICSCalendarUrl = event => {
+  const components = [
+    "BEGIN:VCALENDAR",
+    "VERSION:2.0",
+    "BEGIN:VEVENT",
     `URL:${document.URL}`,
     `DTSTART:${formatTime(event.startsAt)}`,
     `DTEND:${formatTime(event.endsAt)}`,
     `SUMMARY:${event.name}`,
     `DESCRIPTION:${event.details}`,
     `LOCATION:${event.location}`,
-    'END:VEVENT',
-    'END:VCALENDAR',
+    "END:VEVENT",
+    "END:VCALENDAR",
   ];
 
-  return encodeURI(`data:text/calendar;charset=utf8,${components.join('\n')}`);
+  return encodeURI(`data:text/calendar;charset=utf8,${components.join("\n")}`);
 };
 
-export const generateEventURLs = (event) => ({
+export const generateEventURLs = event => ({
   google: buildGoogleCalendarUrl(event),
   outlook: buildOutlookCalendarUrl(event),
-  yahoo: buildYahooCalendarUrl(event),
+  apple: buildAppleCalendarUrl(event),
   ics: buildICSCalendarUrl(event),
 });
